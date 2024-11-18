b5bc55e1b2936d6dde36facc658cfc60cc9e4374
HardsonRichard <120266094+HardsonRichard@users.noreply.github.com>
2024-11-18 17:08:39 +0300

    Authentication added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ProductController;
 use App\Http\Controllers\API\RatingController;
 use Illuminate\Http\Request;
@@ -9,6 +10,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::apiResource('user-ratings', RatingController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('user-ratings', RatingController::class);
+});
 
-Route::apiResource('products', ProductController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('products', ProductController::class);
+});
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register']);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
